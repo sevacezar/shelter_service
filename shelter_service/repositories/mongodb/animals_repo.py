bed659218a5766510bce600f4d8856e97de7854f
sevacezar <kryazhev_seva@mail.ru>
2024-12-16 17:17:05 +0500

    Adding animal mongodb repo methods realizations

--- shelter_service/repositories/mongodb/animals_repo.py
+++ shelter_service/repositories/mongodb/animals_repo.py
@@ -1,3 +1,5 @@
+from typing import Any
+from bson import ObjectId
 from motor.motor_asyncio import AsyncIOMotorDatabase, AsyncIOMotorCollection
 
 from domain.animals import Animal
@@ -10,21 +12,59 @@ class AnimalMongoRepository(AnimalBaseRepository):
     @property
     def collection(self) -> AsyncIOMotorCollection:
         return self._collection
+
+    def _get_animal_obj_with_str_id(self, animal_dict: dict) -> Animal:
+        if animal_dict.get('_id'):
+            animal_dict['id'] = str(animal_dict.pop('_id'))
+        return Animal(**animal_dict)
+
+    async def create(self, animal: Animal) -> Animal:
+        animal_dict: dict = animal.to_dict()
+        del animal_dict['id']
+        res = await self._collection.insert_one(animal_dict)
+        animal.id = str(res.inserted_id)
+        return animal
+
+    async def get_by_id(self, id: str) -> Animal | None:
+        _id: ObjectId = ObjectId(id)
+        res = await self._collection.find_one(filter={'_id': _id})
+        if not res:
+            return None
+        return self._get_animal_obj_with_str_id(animal_dict=res)
+
+    async def update(self, animal: Animal, updated_params: dict[str, Any]) -> Animal:
+        _id: ObjectId = ObjectId(animal.id)
+        updated_animal: Animal = animal.update(params=updated_params)
+        updated_animal_dict: dict = updated_animal.to_dict()
+        del updated_animal_dict['id']
+        res = await self._collection.replace_one(filter={'_id': _id}, replacement=updated_animal_dict)
+        return updated_animal
+
+    async def get_all(self, offset = 0, limit = None) -> list[Any]:
+        animals: list[Animal] = []
+        cursor = self._collection.find()
+        if offset > 0:
+            cursor = cursor.skip(offset)
+        if limit:
+            cursor = cursor.limit(limit)
+        async for doc in cursor:
+            animals.append(self._get_animal_obj_with_str_id(animal_dict=doc))
+        return animals
+
+    async def delete(self, animal: Animal) -> None:
+        _id: ObjectId = ObjectId(animal.id)
+        await self._collection.delete_one(filter={'_id': _id})
+
+    async def get_filtered(self, filters: dict[str, Any], offset = 0, limit = None):
+        animals: list[Animal] = []
+        filter_dict: dict = {key: {'$in': values} for key, values in filters.items()}
+        cursor = self._collection.find(filter=filter_dict)
+        if offset > 0:
+            cursor = cursor.skip(offset)
+        if limit:
+            cursor = cursor.limit(limit)
+        async for doc in cursor:
+            animals.append(self._get_animal_obj_with_str_id(animal_dict=doc))
+        return animals
+
     
-    async def create(self, animal):
-        return await super().create(animal)
-    
-    async def get_by_id(self, id):
-        return await super().get_by_id(id)
-    
-    async def update(self, animal, updated_params):
-        return await super().update(animal, updated_params)
-    
-    async def get_all(self, offset = 0, limit = None):
-        return await super().get_all(offset, limit)
-    
-    async def delete(self, animal):
-        return await super().delete(animal)
-    
-    async def get_filtered(self, filters, offset = 0, limit = None):
-        return await super().get_filtered(filters, offset, limit)
