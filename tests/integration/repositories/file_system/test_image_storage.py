7a23bd75cbd3a34aa497013744f2306e9bfe29ae
sevacezar <kryazhev_seva@mail.ru>
2024-12-18 17:10:13 +0500

    Adding delete integration tests for FS storage

--- tests/integration/repositories/file_system/test_image_storage.py
+++ tests/integration/repositories/file_system/test_image_storage.py
@@ -9,7 +9,7 @@ from config import UPLOAD_DIR_PATH
 from repositories.file_system.image_storage import ImageFsStorage
 
 
-@pytest.fixture(scope='class', autouse=True)
+@pytest.fixture(scope='function', autouse=True)
 def teardown():
     yield
     shutil.rmtree(UPLOAD_DIR_PATH)
@@ -25,11 +25,11 @@ class TestImageStorage:
     test_data_path: str = os.path.abspath(os.path.join(os.path.dirname(__file__), '../../data'))
 
     async def test_save_image(self, image_storage: ImageFsStorage):
+        animal_id: str = str(ObjectId())
         test_filename: str = 'funny_photo.png'
         test_filepath: str = os.path.join(self.test_data_path, test_filename)
         assert os.path.exists(test_filepath)
 
-        animal_id=str(ObjectId())
         async with async_open(test_filepath, 'rb') as input_file:
             file_content: bytes = await input_file.read()
             relative_image_path: str = await image_storage.save(
@@ -49,3 +49,70 @@ class TestImageStorage:
         async with async_open(saved_file_path, 'rb') as file:
             saved_content: bytes = await file.read()
             assert saved_content == file_content
+
+    async def test_save_image_with_repeat_name(self, image_storage: ImageFsStorage):
+        animal_id: str = str(ObjectId())
+        test_filename: str = 'funny_photo.png'
+        test_filepath: str = os.path.join(self.test_data_path, test_filename)
+        assert os.path.exists(test_filepath)
+
+        async with async_open(test_filepath, 'rb') as input_file:
+            file_content: bytes = await input_file.read()
+            await image_storage.save(
+                file_content=file_content,
+                filename=test_filename,
+                animal_id=animal_id,
+            )
+            relative_image_path: str = await image_storage.save(
+                file_content=file_content,
+                filename=test_filename,
+                animal_id=animal_id,
+            )
+
+        base, extenson = os.path.splitext(test_filename)
+        new_filename: str = f'{base}(1){extenson}'
+
+        saved_file_path: str = os.path.abspath(os.path.join(
+            image_storage.image_dir_path,
+            animal_id,
+            new_filename,
+        ))
+
+        assert os.path.exists(saved_file_path)
+        assert relative_image_path.split(os.sep) == [animal_id, new_filename]
+
+        async with async_open(saved_file_path, 'rb') as file:
+            saved_content: bytes = await file.read()
+            assert saved_content == file_content
+
+    async def test_delete_success(self, image_storage: ImageFsStorage):
+        animal_id: str = str(ObjectId())
+        test_filename: str = 'funny_photo.png'
+        test_filepath: str = os.path.join(self.test_data_path, test_filename)
+        assert os.path.exists(test_filepath)
+
+        async with async_open(test_filepath, 'rb') as input_file:
+            file_content: bytes = await input_file.read()
+            relative_image_path: str = await image_storage.save(
+                file_content=file_content,
+                filename=test_filename,
+                animal_id=animal_id,
+            )
+
+        deleting_res: bool = await image_storage.delete(relative_file_path=relative_image_path)
+        assert deleting_res
+        assert not os.path.exists(os.path.abspath(os.path.join(
+            image_storage.image_dir_path,
+            animal_id,
+            test_filename,
+        )))
+
+    async def test_delete_not_found(self, image_storage: ImageFsStorage):
+        animal_id: str = str(ObjectId())
+        os.makedirs(os.path.join(image_storage.image_dir_path, animal_id))
+        test_filename: str = 'funny_photo.png'
+
+        relative_path: str = fr'{animal_id}\{test_filename}'
+
+        deleting_res: bool = await image_storage.delete(relative_file_path=relative_path)
+        assert not deleting_res
